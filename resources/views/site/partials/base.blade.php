56ceed1ac5b50b030e7391cb028be2a4f29634e2
jonathan.penny@gmail.com <jonathan.penny@gmail.com>
2017-08-28 14:34:58 +0100

    Added foundation icon font.

--- resources/views/site/partials/base.blade.php
+++ resources/views/site/partials/base.blade.php
@@ -13,24 +13,25 @@
     <link rel="icon" href="/favicon.ico" type="image/x-icon"/>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
     <!-- Styles -->
+    <link href="{{ asset('css/foundation-icons.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
 <script>
-    (function (i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r;
-        i[r] = i[r] || function () {
-            (i[r].q = i[r].q || []).push(arguments);
-        }, i[r].l = 1 * new Date();
-        a = s.createElement(o),
-            m = s.getElementsByTagName(o)[0];
-        a.async = 1;
-        a.src = g;
-        m.parentNode.insertBefore(a, m);
-    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+  (function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+      (i[r].q = i[r].q || []).push(arguments);
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m);
+  })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
-    ga('create', 'UA-74293771-1', 'auto');
-    ga('send', 'pageview');
+  ga('create', 'UA-74293771-1', 'auto');
+  ga('send', 'pageview');
 
 </script>
 <div id="app">
@@ -55,21 +56,22 @@
     <footer>
         <div class="container">
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-12">
                     <div class="social">
-                        <a href="mailto:jon@jonppenny.co.uk" title="Email">
-                            <img width="32" height="32" src="/images/iconmonstr-email-10.svg" alt="Email"/>
-                        </a>
-                        <a href="http://twitter.com/jonppenny" title="Twitter">
-                            <img width="32" height="32" src="/images/iconmonstr-twitter-4.svg" alt="Twitter"/>
-                        </a>
-                        <a href="http://uk.linkedin.com/in/jonppenny" title="LinkedIn">
-                            <img width="32" height="32" src="/images/iconmonstr-linkedin-4.svg" alt="LinkedIn"/>
-                        </a>
-                        <a href="https://github.com/jonppenny" title="Github">
-                            <img width="32" height="32" src="/images/iconmonstr-github-4.svg" alt="Github"/>
-                        </a>
+                        <a href="mailto:jon@jonppenny.co.uk" title="Email" class="fi-mail"></a>
+                        <a href="http://twitter.com/jonppenny" title="Twitter" class="fi-social-twitter"></a>
+                        <a href="http://uk.linkedin.com/in/jonppenny" title="LinkedIn" class="fi-social-linkedin"></a>
+                        <a href="https://github.com/jonppenny" title="Github" class="fi-social-github"></a>
                     </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <nav class="footer-nav">
+                        <a href="/privacy" title="Privacy Policy">
+                            <small>Privacy Policy</small>
+                        </a>
+                    </nav>
                 </div>
                 <div class="col-md-6 text-right">
                     <p>
