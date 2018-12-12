c4693cc714c09a65c619cf690e980e8766636ef1
Jonathan Penny <jonathan.penny@gmail.com>
2018-12-12 14:49:25 +0000

    Added User Unit tests
    Added unit tests for CRUD user actions.

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -16,9 +16,18 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'name'           => $faker->name,
+        'email'          => $faker->unique()->safeEmail,
+        'password'       => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Page::class, function (Faker\Generator $faker) {
+    return [
+        'title'       => $faker->text,
+        'slug'        => $faker->slug,
+        'template'    => 'default',
+        'the_content' => json_encode($faker->paragraph)
     ];
 });
---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\User;
+use function dd;
+use function factory;
+use function route;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class UserTest extends TestCase
+{
+    public function test_create_user()
+    {
+        $data = [
+            'name'        => $this->faker->name,
+            'email'       => $this->faker->email,
+            'password'    => $this->faker->password,
+            'role'        => 'user',
+            'user_avatar' => '',
+        ];
+
+        $response = $this->post(route('user.store'), $data);
+
+        $response->assertStatus(302);
+    }
+
+    public function test_view_user()
+    {
+        return;
+    }
+
+    public function test_update_user()
+    {
+        $user = factory(User::class)->create();
+
+        $data = [
+            'name'        => $this->faker->name,
+            'email'       => $this->faker->email,
+            'password'    => $this->faker->password,
+            'role'        => 'user',
+            'user_avatar' => '',
+        ];
+
+        $response = $this->patch(route('user.update', $user->id), $data);
+
+        $response->assertStatus(302);
+    }
+
+    public function test_delete_user()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->delete(route('user.delete', $user->id));
+
+        $response->assertStatus(302);
+    }
+}
