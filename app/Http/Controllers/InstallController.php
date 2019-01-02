f3d6cefc5b98ae62a6c18e17f5e45fb0001188a2
Jonathan Penny <jonathan.penny@gmail.com>
2019-01-02 16:44:27 +0000

    Laravel update
    Updated to Laravel 5.7

---
+++ app/Http/Controllers/InstallController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Install;
+use Illuminate\Http\Request;
+
+class InstallController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Install  $install
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Install $install)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Install  $install
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Install $install)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Install  $install
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Install $install)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Install  $install
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Install $install)
+    {
+        //
+    }
+}
